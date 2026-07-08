e1d70379988741fd7a42bf69a6b0e2de9910b2ea
JoeOConnorTIL <jpoc1g11@gmail.com>
2026-07-08 15:43:06 +0100

    Updating script
    Added logic to handle errors

--- extract.py
+++ extract.py
@@ -13,5 +13,20 @@ delay = 10
 response = requests.get(url)
 data = response.json()
 
-with open(filename, 'w') as file:
-    json.dump(data, file)
+while attempt < max_retry:
+    response = requests.get(url)
+    status = response.status_code
+    if 200 <= status < 300:
+        data = response.json()
+        with open(filename, 'w') as file:
+            json.dump(data, file)
+        print('file loaded')
+        break
+    elif status <= 100 or status >=500:
+        time.sleep(delay)
+        print('retrying')
+        attempt += 1
+    else:
+        print('fix something')
+        print(status)
+        break
